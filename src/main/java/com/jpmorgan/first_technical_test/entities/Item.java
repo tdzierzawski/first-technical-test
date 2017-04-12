152a21e6af236456b75a31b529f3f824339874bf
Tomek <Tomek@192.168.1.12>
2017-04-12 20:21:26 +0200

    Added unit entity

---
+++ src/main/java/com/jpmorgan/first_technical_test/entities/Item.java
@@ -0,0 +1,64 @@
+package com.jpmorgan.first_technical_test.entities;
+
+import java.util.Date;
+
+public class Item {
+
+	private String entity;
+	private String buySell;
+	private Double agreedFx;
+	private String currency;
+	private Date instructionDate;
+	private Date settlementDate;
+	private String units;
+	private Double pricePerUnit;
+	
+	public String getEntity() {
+		return entity;
+	}
+	public void setEntity(String entity) {
+		this.entity = entity;
+	}
+	public String getBuySell() {
+		return buySell;
+	}
+	public void setBuySell(String buySell) {
+		this.buySell = buySell;
+	}
+	public Double getAgreedFx() {
+		return agreedFx;
+	}
+	public void setAgreedFx(Double agreedFx) {
+		this.agreedFx = agreedFx;
+	}
+	public String getCurrency() {
+		return currency;
+	}
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+	public Date getInstructionDate() {
+		return instructionDate;
+	}
+	public void setInstructionDate(Date instructionDate) {
+		this.instructionDate = instructionDate;
+	}
+	public Date getSettlementDate() {
+		return settlementDate;
+	}
+	public void setSettlementDate(Date settlementDate) {
+		this.settlementDate = settlementDate;
+	}
+	public String getUnits() {
+		return units;
+	}
+	public void setUnits(String units) {
+		this.units = units;
+	}
+	public Double getPricePerUnit() {
+		return pricePerUnit;
+	}
+	public void setPricePerUnit(Double pricePerUnit) {
+		this.pricePerUnit = pricePerUnit;
+	}
+}
