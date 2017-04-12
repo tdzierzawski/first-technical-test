30f9f96916271c9e84042b13365667d8a3cba90d
Tomek <Tomek@192.168.1.12>
2017-04-12 19:29:51 +0200

    Init commit

---
+++ src/main/java/com/jpmorgan/first_technical_test/App.java
@@ -0,0 +1,13 @@
+package com.jpmorgan.first_technical_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
